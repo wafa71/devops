5e4d14ccc362d0ded84f41f0a0c228172ee2dd58
Sk4nd3r <skanderkahouli26@hotmail.com>
2022-11-16 14:23:50 +0100

    fix tests

---
+++ src/test/java/tn/esprit/rh/achat/FactureServiceImplTest.java
@@ -0,0 +1,28 @@
+package tn.esprit.rh.achat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.services.IFactureService;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class FactureServiceImplTest {
+    @Autowired
+    IFactureService us;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllUsers() {
+        List<Facture> listFactures = us.retrieveAllFactures();
+        Assertions.assertEquals(0, listFactures.size());
+    }
+}
